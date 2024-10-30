0bba6300261cab8e1e3517609b7c5c375a7f1473
LalitPatil0007 <lalitpatil2407@gmail.com>
2024-10-30 23:30:16 +0530

    commit for 1 d array programs

--- src/arrays/NthLargestKthSmallest.java
+++ src/arrays/NthLargestKthSmallest.java
@@ -1,9 +1,20 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class NthLargestKthSmallest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int[] z = {4,5,7,8,9,10};
+		
+		Arrays.sort(z);
+		   System.out.println(" largest element "+ z[z.length-1]);
+	    System.out.println("Second largest element "+ z[z.length-2]);
+	    System.out.println("smallest element "+ z[0]);
+	    System.out.println("2nd smallest element "+ z[1]);
+	    System.out.println("3nd smallest element "+ z[2]);
 
 	}
 
